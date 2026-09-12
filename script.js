40cc142ca35ffc8e7a753c82344fb50c35994c9d
ash <230867782+Ashinaz@users.noreply.github.com>
2026-09-13 05:24:36 +0800

    change if condition from assignment to comparison

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function playRound(humanChoice) {
     )
     let computerWin = (
         (computerChoice == "rock" && humanChoice == "scissors") ||
-        (computerChoice== "paper" && humanChoice == "rock") ||
-        (computerChoice== "scissors" && humanChoice == "paper")
+        (computerChoice == "paper" && humanChoice == "rock") ||
+        (computerChoice == "scissors" && humanChoice == "paper")
     )
     console.log(humanWin)
     console.log(computerWin)
@@ -48,15 +48,15 @@ function playGame(e) {
     let winner = playRound(humanChoice)
     console.log(winner)
     if (rounds < 5) {
-        if (winner = "human") {
+        if (winner == "human") {
             winText.textContent = `Round ${rounds+1}: You are the winner!`
             humanScore++
             rounds++
-        }else if (winner = "computer") {
+        }else if (winner =="computer") {
             winText.textContent = `Round ${rounds+1}: You Lost!`
             computerScore++
             rounds++
-        } else if (winner = "draw") {
+        } else if (winner == "draw") {
             winText.textContent = `Round ${rounds+1}: It's a draw!`
             rounds++
         }
